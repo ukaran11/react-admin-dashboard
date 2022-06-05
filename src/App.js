6ee86e039b7d80c8b4bd37db7e221400894e9ffd
unknown <ukaran11@gmail.com>
2022-06-06 01:10:04 +0530

    Utsav | Added tooltip:

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ import './App.css';
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button 
+                type="button" 
+                className='text-3xl p-3 hover:bg-light-gray text-white'
+                style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
